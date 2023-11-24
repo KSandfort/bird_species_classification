7913bbd7cc5d9454e1392b3e370a0ca0148692b1
KSandfort <konstantin.sandfort@gmail.com>
2023-11-24 13:47:56 +0100

    created notebook, model training

--- efficientNetEvaluation.py
+++ efficientNetEvaluation.py
@@ -8,23 +8,35 @@ if __name__ == '__main__':
 
     # Import data
     train_data = keras.preprocessing.image_dataset_from_directory(
-        "archive/train",
+        'input/train',
         batch_size=64,
-        image_size=(224, 224), shuffle=True
+        image_size=(224, 224),
+        shuffle=True,
+        labels='inferred'
     )
 
     val_data = keras.preprocessing.image_dataset_from_directory(
-        "archive/valid",
+        'input/valid',
         batch_size=64,
-        image_size=(224, 224), shuffle=True
+        image_size=(224, 224),
+        shuffle=True,
+        labels='inferred'
     )
 
     test_data = keras.preprocessing.image_dataset_from_directory(
-        "archive/test",
+        'input/test',
         batch_size=64,
-        image_size=(224, 224), shuffle=True
+        image_size=(224, 224),
+        shuffle=True,
+        labels='inferred'
     )
+
+    #print(test_data)
 
     #print(train_data.class_names)
 
     model = keras.models.load_model('models/EfficientNetB0-525-(224 X 224)- 98.97.h5', custom_objects={'F1_score': 'F1_score'})
+
+    predictions = model.predict(test_data)
+    print(predictions)
+    print(predictions.shape)
