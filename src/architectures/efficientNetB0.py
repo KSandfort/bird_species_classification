12d7cd90e8bc3c3ece1ca2c61235f24b3ba3053b
KSandfort <konstantin.sandfort@gmail.com>
2023-12-03 18:21:34 +0100

    efficientNetB0 tests

--- src/architectures/efficientNetB0.py
+++ src/architectures/efficientNetB0.py
@@ -1,8 +1,33 @@
 import keras
 from keras import layers
+from keras import regularizers
 
 
 def get_efficientNetB0_model(input_shape, num_classes):
+    base_model = keras.applications.efficientnet.EfficientNetB0(
+        include_top=False,
+        weights="imagenet",
+        input_shape=input_shape,
+        pooling='max'
+    )
+
+    base_model.trainable = False
+
+    model = keras.models.Sequential([
+        base_model,
+        layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
+        layers.Dense(
+            256,
+            kernel_regularizer=regularizers.l2(l=0.016),
+            activity_regularizer=regularizers.l1(0.006),
+            bias_regularizer=regularizers.l1(0.006), activation='relu'),
+        keras.layers.Dropout(rate=0.45, seed=123),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    return model
+
+def get_efficientNetB0_model_old(input_shape, num_classes):
     # Load the pretained model
     pretrained_model = keras.applications.efficientnet.EfficientNetB0(
         input_shape=input_shape,
@@ -11,7 +36,7 @@ def get_efficientNetB0_model(input_shape, num_classes):
         pooling='max'
     )
 
-    pretrained_model.trainable = False
+    #pretrained_model.trainable = True
 
     inputs = pretrained_model.input
     x = layers.Dense(128, activation='relu')(pretrained_model.output)
