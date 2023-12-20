77a1ad59476972168e469fd56be0ddd5f1f11fd1
KSandfort <konstantin.sandfort@gmail.com>
2023-12-21 00:58:37 +0100

    overall progress

--- src/training_augmented.py
+++ src/training_augmented.py
@@ -9,13 +9,6 @@ from architectures.efficientNetB0 import get_efficientNetB0_model
 import keras
 import os
 
-
-def add_gaussian_noise(image, stddev=0.1):
-    noise = tf.random.normal(tf.shape(image), mean=0.0, stddev=stddev, dtype=tf.float32)
-    noisy_image = tf.clip_by_value(image + noise, 0.0, 1.0)
-    return noisy_image
-
-
 if __name__ == '__main__':
 
     num_epochs = 50  # Number of training epochs
@@ -26,7 +19,7 @@ if __name__ == '__main__':
     num_classes = 525
 
     # Load data
-    train_data, test_data, val_data = data_loader.get_data(input_path, augmented=False)
+    train_data, test_data, val_data = data_loader.get_data(input_path, augmented=True)
     #train_data = train_data.map(lambda img: add_gaussian_noise(img, stddev=0.1))
 
     model_identifiers = [
@@ -45,15 +38,6 @@ if __name__ == '__main__':
         elif identifier == 'efficientNetB0':
             model = get_efficientNetB0_model(input_shape, num_classes)
 
-        augment = tf.keras.Sequential([
-            tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255),
-            tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
-            tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
-            tf.keras.layers.experimental.preprocessing.RandomZoom(0.1),
-            tf.keras.layers.experimental.preprocessing.RandomContrast(0.1),
-
-        ])
-
         # Compile the model
         model.compile(
             loss="categorical_crossentropy",
@@ -62,8 +46,8 @@ if __name__ == '__main__':
 
         # Get time as a part of the log file name
         now = datetime.now()
-        log_name = f'training_{identifier}_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.log'
-        model_name = f'model_{identifier}_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.keras'
+        log_name = f'training_{identifier}_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}_augmented.log'
+        model_name = f'model_{identifier}_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}_augmented.keras'
 
         # Add logger
         if not os.path.exists(os.path.join(output_path, 'logs')):
